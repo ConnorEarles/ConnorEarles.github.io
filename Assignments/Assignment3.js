daf94d07ae1a8b452e6ea11376523a20ee176b69
ConnorEarles <98667643+ConnorEarles@users.noreply.github.com>
2022-09-29 08:15:08 -0400

    Update Assignment3.js

--- Assignments/Assignment3.js
+++ Assignments/Assignment3.js
@@ -1,1 +1,20 @@
+function GCD(input1,input2){
+    value1 = document.getElementById("input1").value
+    value2 = document.getElementById("input2").value
 
+    if((typeof value1 !== 'number') || (typeof value2 !== 'number'))
+        return false;
+    value1 = Math.abs(value1);
+    value2 = Math.abs(value2);
+
+    while(value2){
+        var t = value2;
+        value2 = value1 % value2
+        value1 = t
+    }
+    value1 = document.getElementById("output").innerHTML
+    
+    
+}
+
+    

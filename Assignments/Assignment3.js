dda60b57f26137a3ac84d757d2820af95817f2e4
ConnorEarles <98667643+ConnorEarles@users.noreply.github.com>
2022-09-29 10:20:42 -0400

    Update Assignment3.js

--- Assignments/Assignment3.js
+++ Assignments/Assignment3.js
@@ -1,20 +1,32 @@
-function GCD(input1,input2){
+function GCD(){
+    console.log("Please Work");
     value1 = document.getElementById("input1").value
     value2 = document.getElementById("input2").value
-
-    if((typeof value1 !== 'number') || (typeof value2 !== 'number'))
-        return false;
-    value1 = Math.abs(value1);
-    value2 = Math.abs(value2);
-
+    console.log(value1, value2);
+   
+    
     while(value2){
         var t = value2;
         value2 = value1 % value2
         value1 = t
+        console.log(t)
     }
-    value1 = document.getElementById("output").innerHTML
-    
-    
+    document.getElementById("output").value = value1;
+
+
+function Sum(){
+     let sum = 0
+     SumNum = document.getElementById(SumNum).value
+     while(SumNum){
+         sum += SumNum % 10;
+         SumNum = Math.floor(SumNum / 10)
+
+    }
+     document.getElementById("SumNum").value = SumNum 
+
+}
+
+
 }
 
     
